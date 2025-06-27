98aab9eec973a0a3f7ea67f1a6eb2ebae42a891f
Aynur <a.davletkulov@modulbank.ru>
2025-06-27 14:39:33 +0500

    feature-fix-auth-page

--- src/app/auth/containers/RegistrationWizard.container.tsx
+++ src/app/auth/containers/RegistrationWizard.container.tsx
@@ -3,7 +3,10 @@ import { RegistrationFormContainer } from "@/app/auth/containers/RegistrationFor
 import { RegistrationStepsContainer } from "@/app/auth/containers/RegistrationSteps.container";
 import { RegistrationConfirmContainer } from "@/app/auth/containers/RegistrationConfirm.container";
 import { RegistrationInnContainer } from "@/app/auth/containers/RegistrationInn.container";
-import type { TRegistrationFormValues } from "../infra/RegistrationForm.infra";
+import type { TRegistrationFormValues } from "../infra/RegistrationForm.infra"; // assets
+import logo from "@/assets/logo.svg";
+
+import { Link } from "@tanstack/react-router";
 
 const RegistrationWizardContainer = () => {
   const [step, setStep] = useState(1);
@@ -19,21 +22,42 @@ const RegistrationWizardContainer = () => {
   return (
     <div className="mx-auto flex flex-col min-h-screen">
       <div className="flex flex-1 bg-white overflow-hidden">
-        {/* Левая часть: шаги */}
-        {step === 1 && (
-          <RegistrationFormContainer
-            onSuccess={handleRegistrationSuccess}
-            initialData={formData}
-          />
-        )}
-        {step === 2 && (
-          <RegistrationConfirmContainer
-            onBack={() => setStep(1)}
-            onNext={() => setStep(3)}
-            email={formData?.email}
-          />
-        )}
-        {step === 3 && <RegistrationInnContainer onBack={() => setStep(2)} />}
+        <div className="flex-1 pt-14 px-12 pb-0 flex flex-col justify-between">
+          {/* Логотип */}
+          <img src={logo} alt="logo" className="h-8 mb-12" />
+
+          <div>
+            <h1 className="text-[36px] font-bold text-[#258AFF] mb-8">
+              Регистрация
+            </h1>
+
+            {step === 1 && (
+              <RegistrationFormContainer
+                onSuccess={handleRegistrationSuccess}
+                initialData={formData}
+              />
+            )}
+            {step === 2 && (
+              <RegistrationConfirmContainer
+                onBack={() => setStep(1)}
+                onNext={() => setStep(3)}
+                email={formData?.email}
+              />
+            )}
+            {step === 3 && (
+              <RegistrationInnContainer onBack={() => setStep(2)} />
+            )}
+          </div>
+
+          {/* Ссылка на авторизацию */}
+          <div className="text-[13px] text-[#B0B7C3] mt-8 mb-6 flex justify-center">
+            Уже есть профиль?{" "}
+            <Link to="/login" className="text-[#258AFF] font-medium">
+              Авторизация
+            </Link>
+          </div>
+        </div>
+
         {/* Правая часть: шаги */}
         <RegistrationStepsContainer currentStep={step} />
       </div>
